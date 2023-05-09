76a47a2e263fbe78a5e0213b98ff85356237f774
Adrian Gutierrez <ir.da21@outlook.com>
2023-05-09 15:31:12 -0600

    tokenizacion completa

--- src/controllers/card.controller.js
+++ src/controllers/card.controller.js
@@ -1,11 +1,34 @@
 import { tokenize,detokenize } from "../utilities/tokenization.js";
-
-export const tokenizeCard = (req,res) =>{
+import modelosInit from '../models/init-models.js'
+import { sequelize } from '../database/database.js'
+let modelos = modelosInit(sequelize)
+export const tokenizeCard = async (req,res) =>{
     let {card_holder,card_number,expiration_date,card_type} = req.body;
-    
+    let response;
+    var date = new Date(); // Now
+    date.setDate(date.getDate() + 30);  
+    let sqlDate = date.toISOString().slice(0, 19).replace('T', ' ');
     let card_token = tokenize(card_number);
     let token_date = tokenize(expiration_date);
-    
-    res.status(200).json({"card_token":card_token,"token_date":token_date})
+    try {
+        response = await modelos.cards.create({
+            card_holder,
+            card_number: card_token,
+            expiration_date:token_date,
+            card_type
+        });
+
+        response = await modelos.tokens.create({
+            token:card_token,
+            creado: Date.now(),
+            expired_at : sqlDate,
+            card_id:response.id_card,
+        });
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({"error": error});
+    }
+    res.status(200).json(response)
 
 }
