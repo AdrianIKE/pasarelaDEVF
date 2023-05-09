3e7a7cb7b0c6c1f5b03df7010630df8ab785d7e3
Adrian Gutierrez <ir.da21@outlook.com>
2023-05-09 13:11:05 -0600

    se agregan modelos

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import rutasCard from './routes/card.routes.js';
-//import { sequelize } from './database/database.js';
+import { sequelize } from './database/database.js';
 
-function main(){
-    //Manejo de excepciones
-    /*try{
+async function main(){
+    
+    try{
         await sequelize.sync({force:false});
         await sequelize.authenticate();
         console.log("Conexion exitosa");
     }catch(error){
         console.log("Conexion fallida, error -> "+error);
-    }*/
+    }
     const app = express(); // Inicializa el servidor
     app.use(express.json()); // Se va a comunicar atraves de JSON
     app.use(express.urlencoded({extended:false})); // Permite o no parametros
