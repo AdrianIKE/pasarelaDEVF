8a487a8c9caa80036d7751cfd7499ac41f7cb60e
Adrian Gutierrez <ir.da21@outlook.com>
2023-05-09 21:35:11 -0600

    final clase

--- src/controllers/card.controller.js
+++ src/controllers/card.controller.js
@@ -1,13 +1,24 @@
 import modelosInit from '../models/init-models.js'
 import {sequelize} from '../database/database.js'
 import { tokenize, detokenize} from '../utilities/tokenization.js'
+import luhn from 'luhn';
 let modelos = modelosInit(sequelize)
 
 //Tokenizar -> Intarcambiar numero por token
 export const tokenizeCard = async (req,res) =>{
     let {card_holder,card_number,expiration_date,card_type} = req.body;
-    let response;
+    
+    if(!luhn.validate(card_number)){
+        res.status(403).json({"error": "Tu tarjeta esta incorrecta"});
+        return;
+    }
 
+    let response;
+    let date = new Date();
+    console.log(date);
+    date.setDate(date.getDate() + 1);
+    let sqlDate = date.toISOString().slice(0,19).replace('T',' '); //<--2022-05-10
+    let token = tokenize(card_number+expiration_date+card_type)
     try {
         response = await modelos.cards.create({
             card_holder,
@@ -15,6 +26,13 @@ export const tokenizeCard = async (req,res) =>{
             expiration_date: tokenize(expiration_date), //<---- En claro -> AES256 sha256
             card_type
         });
+
+        response = await modelos.tokens.create({
+            token,
+            creado: Date.now(),
+            expired_at:sqlDate,
+            card_id: response.id_card
+        })
     } catch (error) {
         console.error(error);
         res.status(500).json({"error": error});
